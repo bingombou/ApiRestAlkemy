a7d50a09458a50448aed3cda0eb866aea1bada3c
lucas.paoliello <lucas.paoliello@globant.com>
2021-08-30 20:34:11 -0300

    Defining basic structure for the project

--- src/main/java/com/alkemy/ong/web/HealthController.java
+++ src/main/java/com/alkemy/ong/web/HealthController.java
@@ -1,4 +1,4 @@
-package com.alkemy.ong.monitor;
+package com.alkemy.ong.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
